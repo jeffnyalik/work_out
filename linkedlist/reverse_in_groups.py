1dd3c3f9bd07189a2a57248e9cc53d6f84147167
jeffnyak@gmail.com <jeffnyak@gmail.com>
2023-03-11 09:35:39 +0300

    Reverse Nodes in k-Group

---
+++ linkedlist/reverse_in_groups.py
@@ -0,0 +1,57 @@
+"""
+Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+Example 2:
+
+
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+ 
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 5000
+0 <= Node.val <= 1000
+ 
+
+Follow-up: Can you solve the problem in O(1) extra memory space?
+"""
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if head is None or k == 1: return head
+        
+        current = head
+        next = None
+        prev = None
+        count = 0
+
+        while current !=None and count < k:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+            count +=1
+
+        if count < k:
+            return self.reverseKGroup(prev, count)
+        else:
+            head.next = self.reverseKGroup(next, k)
+        return prev
+
